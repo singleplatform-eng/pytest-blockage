81ebe488941fa7d166cc274bcbda4ca167f3ae19
William Walter <wwalter@singleplatform.com>
2018-10-24 11:48:25 -0400

    add required fields for python setup.py check

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import setup
 
 setup(
     name='pytest-blockage',
+    url='https://github.com/singleplatform-eng/pytest-blockage',
     version='0.3.0+singleplatform.1',
     description='Disable network requests during a test run.',
     long_description=(open('README.rst').read() +
                       open('CHANGELOG.rst').read()),
     license='BSD',
+    author='SinglePlatform Engineering Team',
+    author_email='techservices@singleplatform.com',
     install_requires=['pytest'],
     py_modules=['pytest_blockage'],
     entry_points={'pytest11': ['blockage = pytest_blockage']},
@@ -17,5 +20,6 @@ setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Operating System :: OS Independent',
+        'Private :: Do Not Upload',
     ]
 )
